3f3b19e72fc4108a028ca34afe6a6dab44883769
Anslem1 <andrewanslem1@gmail.com>
2024-07-02 00:35:16 +0100

    Correct class name and installation steps in transit-cli formula

--- Formula/transit-cli.rb
+++ Formula/transit-cli.rb
@@ -1,4 +1,4 @@
-class TransitCli < Formula
+class transit < Formula
   desc "Transit is a versatile command-line interface (CLI) tool designed to streamline the workflow of developers by allowing them to store and execute a series of frequently used terminal commands all at once. This tool is ideal for automating repetitive tasks, thereby saving valuable time and increasing productivity."
   homepage "https://github.com/Anslem1/transit-cli"
   url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.0/transit_Darwin_arm64.tar.gz"
