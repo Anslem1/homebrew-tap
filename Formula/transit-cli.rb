8180f44a36a9d75be043e3196ea39371940ff4bf
Anslem1 <andrewanslem1@gmail.com>
2024-07-02 00:33:33 +0100

    Correct class name and installation steps in transit-cli formula

--- Formula/transit-cli.rb
+++ Formula/transit-cli.rb
@@ -1,17 +1,15 @@
-class YourCli < Formula
+class TransitCli < Formula
   desc "Transit is a versatile command-line interface (CLI) tool designed to streamline the workflow of developers by allowing them to store and execute a series of frequently used terminal commands all at once. This tool is ideal for automating repetitive tasks, thereby saving valuable time and increasing productivity."
   homepage "https://github.com/Anslem1/transit-cli"
   url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.0/transit_Darwin_arm64.tar.gz"
   sha256 "0a8047d0e24a64c1d8bd8c4d78194224af0e9f1cc985cc4057cbf97d1b950961"
- license "Apache-2.0"
-
-  depends_on "go" => :build
+  license "Apache-2.0"
 
   def install
-    system "go", "build", *std_go_args
+    bin.install "transit"
   end
 
   test do
-    system "#{bin}/your_cli", "--version"
+    system "#{bin}/transit", "--version"
   end
 end
