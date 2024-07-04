73779ed73cd96acd568646ada23d1672c5f310e8
Anslem1 <andrewanslem1@gmail.com>
2024-07-04 20:19:52 +0100

    update

--- Formula/transit-cli.rb
+++ Formula/transit-cli.rb
@@ -1,30 +1,27 @@
 class TransitCli < Formula
-  desc "Transit is a versatile command-line interface (CLI) tool designed to streamline the workflow of developers."
+  desc "Transit is a versatile command-line interface (CLI) tool designed to streamline the workflow of developers in the terminal."
   homepage "https://github.com/Anslem1/transit-cli"
   
-  # Update the URL and SHA256 to the latest release
-  url "https://github.com/Anslem1/transit-cli/releases/download/v1.2.3/transit_Darwin_x86_64.tar.gz"
-  sha256 "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
-  
-  # License of the software
   license "Apache-2.0"
   
-  # Specify installation steps
+url "https://github.com/Anslem1/transit-cli/releases/download/#{version}/transit_darwin_amd64.tar.gz"
+  sha256 "{darwin_amd64_sha256}"
+  url "https://github.com/Anslem1/transit-cli/releases/download/#{version}/transit_darwin_arm64.tar.gz"
+  sha256 "{darwin_arm64_sha256}"
+  url "https://github.com/Anslem1/transit-cli/releases/download/#{version}/transit_linux_amd64.tar.gz"
+  sha256 "{linux_amd64_sha256}"
+  url "https://github.com/Anslem1/transit-cli/releases/download/#{version}/transit_linux_arm64.tar.gz"
+  sha256 "{linux_arm64_sha256}"
+  url "https://github.com/Anslem1/transit-cli/releases/download/#{version}/transit_windows_amd64.tar.gz"
+  sha256 "{windows_amd64_sha256}"
+  url "https://github.com/Anslem1/transit-cli/releases/download/#{version}/transit_windows_arm64.tar.gz"
+  sha256 "{windows_arm64_sha256}"
+
   def install
-    # Install the transit binary into the 'bin' directory
     bin.install "transit"
   end
-  
-  # Test block to verify installation
+
   test do
-    # Run transit with the version flag and check for output
-    assert_match /^v\d+\.\d+\.\d+$/, shell_output("#{bin}/transit version")
+    system "#{bin}/transit", "--version"
   end
-  
-  # Specify bottle block if needed for bottle creation
-  # bottle do
-  #   root_url "https://github.com/Anslem1/transit-cli/releases/download/v1.2.3"
-  #   cellar :any_skip_relocation
-  #   sha256 "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef" => :catalina
-  # end
 end
