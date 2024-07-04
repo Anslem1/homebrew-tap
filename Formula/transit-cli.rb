bacc25519e74eccdb507114e44c639bfd7d854bc
Anslem1 <andrewanslem1@gmail.com>
2024-07-04 15:10:05 +0100

    updated brew file

--- Formula/transit-cli.rb
+++ Formula/transit-cli.rb
@@ -1,4 +1,5 @@
 class TransitCli < Formula
+<<<<<<< HEAD
   desc "Transit is a versatile command-line interface (CLI) tool designed to streamline the workflow of developers by allowing them to store and execute a series of frequently used terminal commands all at once. This tool is ideal for automating repetitive tasks, thereby saving valuable time and increasing productivity."
   homepage "https://github.com/Anslem1/transit-cli"
 
@@ -41,9 +42,38 @@ class TransitCli < Formula
       system "unzip", "transit_Windows_x86_64.zip"
       bin.install "transit.exe"
     end
+=======
+  desc "Transit is a versatile command-line interface (CLI) tool designed to streamline the workflow of developers."
+  homepage "https://github.com/Anslem1/transit-cli"
+  
+  # Update the URL and SHA256 to the latest release
+  url "https://github.com/Anslem1/transit-cli/releases/download/v1.2.3/transit_Darwin_x86_64.tar.gz"
+  sha256 "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+  
+  # License of the software
+  license "Apache-2.0"
+  
+  # Specify installation steps
+  def install
+    # Install the transit binary into the 'bin' directory
+    bin.install "transit"
+>>>>>>> 79d0c0d (updated brew file)
   end
-
+  
+  # Test block to verify installation
   test do
+<<<<<<< HEAD
     system "#{bin}/transit", "--version"
+=======
+    # Run transit with the version cmd and check for output
+    assert_match /^v\d+\.\d+\.\d+$/, shell_output("#{bin}/transit version")
+>>>>>>> 79d0c0d (updated brew file)
   end
+  
+  # Specify bottle block if needed for bottle creation
+  # bottle do
+  #   root_url "https://github.com/Anslem1/transit-cli/releases/download/v1.2.3"
+  #   cellar :any_skip_relocation
+  #   sha256 "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef" => :catalina
+  # end
 end
