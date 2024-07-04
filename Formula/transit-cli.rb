08ca1762a5cdc63559b2774f294c51f68fd637b3
Anslem1 <andrewanslem1@gmail.com>
2024-07-04 20:14:27 +0000

    Update transit-cli to v1.0.1

--- Formula/transit-cli.rb
+++ Formula/transit-cli.rb
@@ -1,23 +1,23 @@
 class TransitCli < Formula
   desc "Transit is a versatile command-line interface (CLI) tool designed to streamline the workflow of developers in the terminal."
   homepage "https://github.com/Anslem1/transit-cli"
-  url "https://github.com/Anslem1/transit-cli/releases/download/#{version}/transit_darwin_amd64.tar.gz"
-  sha256 "{DARWIN_AMD64_SHA256}"
+  url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.1/transit_darwin_amd64.tar.gz"
+  sha256 "53de625ca2ad2bb67a32bae518a179c96ed83529f793bcd2e7507a4fe9e7f140"
 
-  url "https://github.com/Anslem1/transit-cli/releases/download/#{version}/transit_darwin_arm64.tar.gz"
-  sha256 "{DARWIN_ARM64_SHA256}"
+  url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.1/transit_darwin_arm64.tar.gz"
+  sha256 "53de625ca2ad2bb67a32bae518a179c96ed83529f793bcd2e7507a4fe9e7f140"
 
-  url "https://github.com/Anslem1/transit-cli/releases/download/#{version}/transit_linux_amd64.tar.gz"
-  sha256 "{LINUX_AMD64_SHA256}"
+  url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.1/transit_linux_amd64.tar.gz"
+  sha256 "53de625ca2ad2bb67a32bae518a179c96ed83529f793bcd2e7507a4fe9e7f140"
 
-  url "https://github.com/Anslem1/transit-cli/releases/download/#{version}/transit_linux_arm64.tar.gz"
-  sha256 "{LINUX_ARM64_SHA256}"
+  url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.1/transit_linux_arm64.tar.gz"
+  sha256 "53de625ca2ad2bb67a32bae518a179c96ed83529f793bcd2e7507a4fe9e7f140"
 
-  url "https://github.com/Anslem1/transit-cli/releases/download/#{version}/transit_windows_amd64.tar.gz"
-  sha256 "{WINDOWS_AMD64_SHA256}"
+  url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.1/transit_windows_amd64.tar.gz"
+  sha256 "53de625ca2ad2bb67a32bae518a179c96ed83529f793bcd2e7507a4fe9e7f140"
 
-  url "https://github.com/Anslem1/transit-cli/releases/download/#{version}/transit_windows_arm64.tar.gz"
-  sha256 "{WINDOWS_ARM64_SHA256}"
+  url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.1/transit_windows_arm64.tar.gz"
+  sha256 "53de625ca2ad2bb67a32bae518a179c96ed83529f793bcd2e7507a4fe9e7f140"
 
   license "Apache-2.0"
 
