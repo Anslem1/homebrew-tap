016262c87e8ce2ef56f095ca03b445475952d406
Anslem1 <andrewanslem1@gmail.com>
2024-07-05 01:40:26 +0100

    to try

--- Formula/transit-cli.rb
+++ Formula/transit-cli.rb
@@ -5,27 +5,28 @@ class TransitCli < Formula
 
   # macOS amd64
   url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.1/transit_darwin_amd64.tar.gz"
-   sha256 "391a482326c860a2302facd97c368ac47848b35fe1d45e1ca5d607fd244610b7"
+   sha256 ""
 
   # macOS arm64
   url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.1/transit_darwin_arm64.tar.gz"
-   sha256 "3126abf91a66229195f6ce042ad8c352dcbbd32b6303ee12707943426cffbf12"
+   sha256 ""
 
   # Linux amd64
   url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.1/transit_linux_amd64.tar.gz"
-  sha256 "4f2dc1efedb04c45e28ba800996a89b6ed4453701a774e8f661000075e6b055a"
+  sha256 ""
 
 
   # Linux arm64
   url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.1/transit_linux_arm64.tar.gz"
-   sha256 "597f1dc170bd08496d6eb1180c01107c0bab9b1af3fb8d8d8abeea7e94f76a9f"
+   sha256 ""
+
   # Windows amd64
   url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.1/transit_windows_amd64.tar.gz"
-   sha256 "8a45fb0237bdc8689f97e18fecfb9761539f2661b32e26c5b3f798e19e0d1aff"
+   sha256 ""
 
   # Windows arm64
   url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.1/transit_windows_arm64.tar.gz"
-   sha256 "9ed32148ffcb24075ef336178d0e52afecba14258a5375e50103437f6a0511ce"
+   sha256 ""
   
 
   def install
