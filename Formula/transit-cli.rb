f7d3b076c9847eb522d46804539d1605328380cc
Anslem1 <andrewanslem1@gmail.com>
2024-07-14 23:31:07 +0000

    Update transit-cli to v1.0.1

--- Formula/transit-cli.rb
+++ Formula/transit-cli.rb
@@ -6,18 +6,22 @@ class TransitCli < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.1/transit_darwin_amd64.tar.gz"
+  sha256 "9ed56cadc72a3015e4d4e8ac8dd5132bdd346ed1c9b8fd1f071ea26ac339f5aa"
 
    
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.1/transit_darwin_arm64.tar.gz"
+  sha256 "58c5d7db5fa3fd651f39eac3b45cd82562c56aaffc66e4ed89fafa61eb3ff467"
 
   
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.1/transit_linux_amd64.tar.gz"
+  sha256 "4474c92bd4de34c895a94bc701aa2a8d5a1390bc5a20fbb7de387bbd0bfeee26"
   
 
   elsif OS.linux? && Hardware::CPU.arm?
     url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.1/transit_linux_arm64.tar.gz"
+  sha256 "e3046ebd0ad7f902a879daa671aac9b46712416d343cc742ab592828faf26205"
   
 
   elsif OS.windows? && Hardware::CPU.intel?
