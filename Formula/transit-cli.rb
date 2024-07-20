0f1104786e0c2b589fed630168877bd550c1a421
Anslem1 <Anslem1@users.noreply.github.com>
2024-07-20 18:29:08 +0000

    Update Homebrew formula for  v1.0.0

--- Formula/transit-cli.rb
+++ Formula/transit-cli.rb
@@ -3,25 +3,25 @@ class TransitCli < Formula
   homepage "https://github.com/Anslem1/transit-cli"
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.0/transit-cli_v1.0.0_darwin_amd64.tar.gz"
-    sha256 "fa09aa55de1da63490653e3028c5c1edce0de1520a59cd837af363399c499f5a"
+    sha256 "c9bde99de538de2c6a325374cf431b2b17f70db6a5137171fe93c8ec534cc131"
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.0/transit-cli_v1.0.0_darwin_arm64.tar.gz"
-    sha256 "0aff963987b7baebaac24f1fbe8aad1654141aec555f7d9f114066b9c50ea257"
+    sha256 "92d5ed2e65ce1fa2470d5c915f3c352e1ae5d6ef9b7fe1598fbd44a13db86903"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.0/transit-cli_v1.0.0_linux_amd64.tar.gz"
-    sha256 "8711e3ee737e3f80f071f373388e76d5b4fa2fbd5b8101e149167076726a9cf1"
+    sha256 "dea128f964ae0a67e438a66bdbf925100434c58e0915a1490d7e7abc09c5da4e"
   elsif OS.linux? && Hardware::CPU.arm?
     url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.0/transit-cli_v1.0.0_linux_arm64.tar.gz"
-    sha256 "b7c2066e6a7a37d51790baf51624ee70615cf1b08f4cfcd506e3e2b0c3c7c5ca"
+    sha256 "9f08bc70814211710c0b7dde4351656f30ee30ae5df99d5bed1377e320b44b3e"
   elsif OS.windows? && Hardware::CPU.intel?
     url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.0/transit-cli_v1.0.0_windows_amd64.zip"
-    sha256 "424fd2092a8c01c3ec062d23d71dfb66038b97227850d09eabca4f1e50ad9cf8"
+    sha256 "b53d99fa2dd42a42580f3f1e8962db116d1e2a179954ae4271c89c40060e9d81"
   elsif OS.windows? && Hardware::CPU.arm?
     url "https://github.com/Anslem1/transit-cli/releases/download/v1.0.0/transit-cli_v1.0.0_windows_arm64.zip"
-    sha256 "c4d31d727df62b8f5fd51f1ccc332b7b731b2641b2d0fad63c04953921337036"
+    sha256 "513c9f1dc84bcb95c4a2fc7a17ac5ede2617bcdb4c79d756a3956bd81f3c01f8"
   end
   def install
-    bin.install "transit-cli"
+    bin.install "transit-cli" => "transit"
   end
   test do
     system "#{bin}/transit", "--version"
