410d74cb3f4d3655b2c0660108c535a57d8073ff
Chidiebube Anyanwu <andrewanslem1@gmail.com>
2026-09-17 22:11:43 +0100

    Add explicit version 1.1.0 to formula

--- Formula/transit-cli.rb
+++ Formula/transit-cli.rb
@@ -1,6 +1,8 @@
 class TransitCli < Formula
   desc "Transit is an interactive CLI tool designed to streamline developer workflows with sequential and concurrent command execution."
   homepage "https://github.com/Anslem1/transit-cli"
+  version "1.1.0"
+
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/Anslem1/transit-cli/releases/download/v1.1.0/transit-cli_v1.1.0_darwin_amd64.tar.gz"
     sha256 "f7508f6677391dc7301994d096840ea6a98febdfb1f89feee4e642e41be67fc8"
@@ -14,9 +16,11 @@ class TransitCli < Formula
     url "https://github.com/Anslem1/transit-cli/releases/download/v1.1.0/transit-cli_v1.1.0_linux_arm64.tar.gz"
     sha256 "5f325f7533ca444747f34a555938891a1d71d71c28261bb74b9ed56ed49159af"
   end
+
   def install
     bin.install "transit-cli" => "transit"
   end
+
   test do
     system "#{bin}/transit", "version"
   end
